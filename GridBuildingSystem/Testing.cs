08f72adc57c57e1e2cb73eb9da7b9fd85dcff8bf
AIdan Bachelez <aidan.bachelez@gmail.com>
2025-06-07 15:53:32 +0200

    initial commit

---
+++ GridBuildingSystem/Testing.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private HeatMapVisual heatMapVisual;
+    [SerializeField] private HeatMapBoolVisual heatMapBoolVisual;
+    [SerializeField] private HeatMapGenericVisual heatMapGenericVisual;
+    private Grid<HeatMapGridObject> grid;
+
+    private void Start()
+    {
+        grid = new Grid<HeatMapGridObject>(20, 10, 10f, Vector3.zero, (Grid<HeatMapGridObject> g, int x, int y) => new HeatMapGridObject(g, x, y));
+
+        //heatMapVisual.SetGrid(grid);
+        //heatMapBoolVisual.SetGrid(grid);
+        heatMapGenericVisual.SetGrid(grid);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = UtilsClass.GetMouseWorldPosition();
+            HeatMapGridObject heatMapGridObject = grid.GetGridObject(position);
+            if (heatMapGridObject != null)
+                heatMapGridObject.AddValue(5);
+        }
+    }
+}
+
+public class HeatMapGridObject
+{
+    private const int MIN = 0;
+    private const int MAX = 100;
+
+    private Grid<HeatMapGridObject> grid;
+    private int x;
+    private int y;
+    private int value;
+
+    public HeatMapGridObject(Grid<HeatMapGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void AddValue(int addValue)
+    {
+        value += addValue;
+        value = Mathf.Clamp(value, MIN, MAX);
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public float GetValueNormalized() { return (float)value / MAX; }
+
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+}
