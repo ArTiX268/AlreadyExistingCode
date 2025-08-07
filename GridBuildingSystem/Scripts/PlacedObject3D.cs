79a3ccf2f81880e8047ccb0c8916f14eed0d0610
Aidan Bachelez <aidan.bachelez@gmail.com>
2025-08-07 07:14:10 +0200

    Added direction enum to placed object 3D

--- GridBuildingSystem/Scripts/PlacedObject3D.cs
+++ GridBuildingSystem/Scripts/PlacedObject3D.cs
@@ -1,6 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction
+{
+    Down,
+    Right,
+    Up,
+    Left
+}
+
 public class PlacedObject3D : MonoBehaviour
 {
     [HideInInspector] public List<Vector2Int> gridPositionList;
