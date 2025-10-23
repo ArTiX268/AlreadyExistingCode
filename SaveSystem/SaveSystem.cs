93557c589955e3cf3d4310c8cf57e074a74c7cab
AIdan Bachelez <aidan.bachelez@gmail.com>
2025-10-23 16:03:03 +0200

    strengthen save system

--- SaveSystem/SaveSystem.cs
+++ SaveSystem/SaveSystem.cs
@@ -3,35 +3,59 @@ using UnityEngine;
 
 public static class SaveSystem
 {
-    private static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
+    private static readonly string SAVE_FOLDER_PATH = Application.dataPath + "/Saves/";
     private static readonly string SAVE_FILE_NAME = "save_";
-    private static readonly string SAVE_FILE_EXTENSION = ".txt";
+    private static readonly string SAVE_FILE_EXTENSION = ".save";
+    private static readonly string META_EXTENSION = ".meta";
 
-    private static void Initialization()
+    public static void Initialization()
     {
-        if (!Directory.Exists(SAVE_FOLDER))
+        if (!Directory.Exists(SAVE_FOLDER_PATH))
         {
-            Directory.CreateDirectory(SAVE_FOLDER);
+            Directory.CreateDirectory(SAVE_FOLDER_PATH);
         }
     }
 
+    #region Save
+
     public static void Save(object pObject)
     {
-        Initialization();
-
         int lSaveNumber = 1;
-        while (File.Exists(SAVE_FOLDER + SAVE_FILE_NAME + lSaveNumber + SAVE_FILE_EXTENSION))
+        while (File.Exists(GetFilePath(SAVE_FILE_NAME + lSaveNumber)))
         {
             lSaveNumber++;
         }
 
-        string lJson = JsonUtility.ToJson(pObject);
-        File.WriteAllText(path: SAVE_FOLDER + SAVE_FILE_NAME + lSaveNumber + SAVE_FILE_EXTENSION, contents: lJson);
+        string lJson = JsonUtility.ToJson(pObject, true);
+        File.WriteAllText(path: GetFilePath(SAVE_FILE_NAME + lSaveNumber), contents: lJson);
     }
 
-    public static T Load<T>()
+    public static void Save(object pObject, string pFileName)
     {
-        DirectoryInfo lDirectoryInfo = new DirectoryInfo(SAVE_FOLDER);
+        string lJson = JsonUtility.ToJson(pObject, true);
+        File.WriteAllText(GetFilePath(pFileName), lJson);
+    }
+
+    public static void Save(object pObject, out string pFilePath)
+    {
+        int lSaveNumber = 1;
+        while (File.Exists(GetFilePath(SAVE_FILE_NAME + lSaveNumber)))
+        {
+            lSaveNumber++;
+        }
+
+        string lJson = JsonUtility.ToJson(pObject, true);
+        File.WriteAllText(path: SAVE_FOLDER_PATH + SAVE_FILE_NAME + lSaveNumber + SAVE_FILE_EXTENSION, contents: lJson);
+        pFilePath = SAVE_FOLDER_PATH + SAVE_FILE_NAME + lSaveNumber + SAVE_FILE_EXTENSION;
+    }
+
+    #endregion
+
+    #region Load
+
+    public static T LoadMostRecentFile<T>()
+    {
+        DirectoryInfo lDirectoryInfo = new DirectoryInfo(SAVE_FOLDER_PATH);
         // The star is used to get all the files of the specified extension
         FileInfo[] lFilesInfo = lDirectoryInfo.GetFiles("*" + SAVE_FILE_EXTENSION);
         FileInfo lMostRecentFile = null;
@@ -50,15 +74,50 @@ public static class SaveSystem
 
         if (lMostRecentFile != null)
         {
-            return Load<T>(lMostRecentFile.FullName);
+            return LoadFile<T>(lMostRecentFile.FullName);
         }
         else
             return default;
     }
 
-    public static T Load<T>(string lFilePath)
+    public static T LoadFile<T>(string pFileName)
     {
-        string lSaveString = File.ReadAllText(lFilePath);
-        return JsonUtility.FromJson<T>(lSaveString);
+        if (File.Exists(GetFilePath(pFileName)))
+        {
+            string lSaveString = File.ReadAllText(GetFilePath(pFileName));
+            return JsonUtility.FromJson<T>(lSaveString);
+        }
+        return default;
     }
+
+    public static T LoadFileAtPath<T>(string pPath)
+    {
+        if (File.Exists(pPath))
+        {
+            string lSaveString = File.ReadAllText(pPath);
+            return JsonUtility.FromJson<T>(lSaveString);
+        }
+        else
+        {
+            Debug.Log("File path invalid.");
+            return default;
+        }
+    }
+
+    #endregion
+
+    public static void DeleteSaveFileByName(string pFileName)
+    {
+        File.Delete(GetFilePath(pFileName));
+        File.Delete(GetFilePath(pFileName) + META_EXTENSION);
+    }
+
+    public static void DeleteSaveFileByPath(string pFilePath)
+    {
+        File.Delete(pFilePath);
+        pFilePath += META_EXTENSION;
+        File.Delete(pFilePath);
+    }
+
+    private static string GetFilePath(string pFileName) => SAVE_FOLDER_PATH + pFileName + SAVE_FILE_EXTENSION;
 }
